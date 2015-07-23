a70568ed189aa332d46b098b1b3f82188c4cfe04
Cameron Manavian <cameron.manavian@gmail.com>
2015-07-23 07:56:09 -0700

    Removed repetitive unit test

--- test/matchBruteForceTest.js
+++ test/matchBruteForceTest.js
@@ -71,61 +71,6 @@ console.log('\nstarting matchBruteForce ...\n');
 
         //[Test]
         (function () {
-            curTest = 'getCol345 7x7';
-            // arrange
-            var match = new Match(),
-                matrix = [
-                    [1, 1, 1, 3, 2, 2, 2],
-                    [2, 3, 4, 5, 1, 1, 2],
-                    [2, 4, 4, 4, 4, 1, 2],
-                    [2, 3, 3, 3, 1, 4, 3],
-                    [2, 3, 5, 3, 1, 4, 3],
-                    [2, 3, 2, 3, 1, 4, 3],
-                    [5, 5, 5, 5, 5, 5, 5]
-                ],
-                expected = [
-                    {
-                        'a': [1, 0],
-                        'b': [5, 0]
-                    },
-                    {
-                        'a': [3, 1],
-                        'b': [5, 1]
-                    },
-                    {
-                        'a': [3, 3],
-                        'b': [5, 3]
-                    },
-                    {
-                        'a': [3, 4],
-                        'b': [5, 4]
-                    },
-                    {
-                        'a': [3, 5],
-                        'b': [5, 5]
-                    },
-                    {
-                        'a': [0, 6],
-                        'b': [2, 6]
-                    },
-                    {
-                        'a': [3, 6],
-                        'b': [5, 6]
-                    }
-                ];
-
-            match.init(matrix);
-
-            // act
-            var result = match.getCol345();
-
-            // assert
-            Assert.deepEqual(result, expected);
-            console.log(curTest + ':PASSED');
-        }());
-
-        //[Test]
-        (function () {
             curTest = 'get345 7x7';
             // arrange
             var match = new Match(),
